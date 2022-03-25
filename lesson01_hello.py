de403ecbffe82e73a015d87eae146d573bfbdb06
JJ-Chen <yanshanfire@foxmail.com>
2022-03-25 20:44:17 +0800

    Init Project

---
+++ lesson01_hello.py
@@ -0,0 +1,9 @@
+import cv2
+
+print(cv2.getVersionString())
+
+image = cv2.imread("./imgs/opencv_logo.jpg")
+print(image.shape)
+
+cv2.imshow("image", image)
+cv2.waitKey()
---
+++ lesson03_crop.py
@@ -0,0 +1,8 @@
+import cv2
+
+image = cv2.imread("imgs/opencv_logo.jpg")
+
+crop = image[10:170, 40:200]
+
+cv2.imshow("crop", crop)
+cv2.waitKey()
